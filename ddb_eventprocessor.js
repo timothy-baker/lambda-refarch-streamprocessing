9fde4c7ec7e2f2e9e17acf874567e2b3118e30c7
Mike Deck <mikedeck@amazon.com>
2016-05-04 23:25:12 -0500

    updating processor function to use config table

--- ddb_eventprocessor.js
+++ ddb_eventprocessor.js
@@ -18,12 +18,32 @@ console.log('Loading function');
 var AWS = require('aws-sdk');
 
 var doc = new AWS.DynamoDB.DocumentClient();
+var config;
 
 exports.handler = function(event, context) {
+  if (config) {
+    handleEvent(event, context);
+  } else {
+    var params = {
+      TableName: 'StreamProcessingRefArchConfig',
+      Key: { Environment: 'demo' }
+    };
+    doc.get(params, function(err, data) {
+      if (err) {
+        console.log(err, err.stack);
+        context.fail(err);
+      } else {
+        config = data.Item;
+        handleEvent(event, context);
+      }
+    });
+  }
+};
+
+function handleEvent(event, context) {
   console.log('Received event:', JSON.stringify(event, null, 2));
 
-  var stackName = context.functionName.split('-').slice(0, -2).join('-');
-  var tableName = stackName + '-EventData';
+  var tableName = config['EventDataTable'];
 
   var putItems = [];
 
@@ -56,7 +76,7 @@ exports.handler = function(event, context) {
   var tableItems = {};
   tableItems[tableName] = putItems;
   writeItems(tableItems, 0, context);
-};
+}
 
 function writeItems(items, retries, context) {
   doc.batchWrite({ RequestItems: items },
